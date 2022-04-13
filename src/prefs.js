036aabf04163b9dcff2f465fd076e947a38e9ca9
Alexandre Del Bigio <alex@cosinus.org>
2022-04-13 12:29:37 +0200

    path only shows if custom-path == true

--- src/prefs.js
+++ src/prefs.js
@@ -3,28 +3,8 @@
 const { Adw, Gio, Gtk } = imports.gi;
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
-// const DesktopIconsUtil = imports.desktopIconsUtil;
 
 function init() {
-}
-
-function createBoolean(settings, nfo) {
-    const row = new Adw.ActionRow({ title: nfo.title });
-    const widget = new Gtk.Switch({
-        active: settings.get_boolean(nfo.name),
-        valign: Gtk.Align.CENTER,
-    });
-    settings.bind(
-        nfo.name,
-        widget,
-        'active',
-        Gio.SettingsBindFlags.DEFAULT
-    );
-
-    row.add_suffix(widget);
-    row.activatable_widget = widget;
-
-    return (row);
 }
 
 function folderChooser(window, settings, callbak) {
@@ -57,35 +37,64 @@ function updatePath(settings, row) {
     }
 }
 
+function createIconGroup(group, settings) {
+    const row = new Adw.ActionRow({ title: 'Use dark icon' });
+    const widget = new Gtk.Switch({
+        active: settings.get_boolean('dark'),
+        valign: Gtk.Align.CENTER,
+    });
+    settings.bind('dark', widget, 'active', Gio.SettingsBindFlags.DEFAULT);
+
+    row.add_suffix(widget);
+    row.activatable_widget = widget;
+
+    group.add(row);
+}
+
 function fillPreferencesWindow(window) {
     const settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.nextcloud-folder');
-    log(settings);
-
     const page = new Adw.PreferencesPage();
     const group = new Adw.PreferencesGroup();
     page.add(group);
 
-    group.add(createBoolean(settings, {
-        title: 'Use dark icon',
-        name: 'dark'
-    }));
-    group.add(createBoolean(settings, {
-        title: 'Use custom Nextcloud path',
-        name: 'custom-path'
-    }));
+    createIconGroup(group, settings);
 
-    const row = new Adw.ActionRow({ title: "Custom Folder Path" });
+    ////////////////////////////////////////////////////
+    // Custom Folder
+    ////////////////////////////////////////////////////
+
+    let row = new Adw.ActionRow({ title: 'Use custom path' });
+    let custom = new Gtk.Switch({
+        active: settings.get_boolean('custom-path'),
+        valign: Gtk.Align.CENTER,
+    });
+    settings.bind('custom-path', custom, 'active', Gio.SettingsBindFlags.DEFAULT);
+
+    row.add_suffix(custom);
+    row.activatable_widget = custom;
+    group.add(row);
+
+    ////////////////////////////////////////////////////
+    // Custom Path
+    ////////////////////////////////////////////////////
+
+    const folder = new Adw.ActionRow({ title: "Folder path" });
     const button = new Gtk.Button({ iconName: 'document-open-symbolic' });
-    updatePath(settings, row);
+    updatePath(settings, folder);
 
     button.connect('clicked', () => {
         folderChooser(window, settings, () => {
-            updatePath(settings, row);
+            updatePath(settings, folder);
         });
     });
-    row.add_suffix(button);
-    row.activatable_widget = button;
-    group.add(row);
+    folder.add_suffix(button);
+    folder.activatable_widget = button;
+
+    folder.visible = settings.get_boolean('custom-path');
+    custom.connect("state-set", () => {
+        folder.visible = !custom.get_state();
+    });
+    group.add(folder);
 
     window.add(page);
 }
